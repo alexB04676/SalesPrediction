a45fe8dc2a7eb1aef629bb2a8cbe37353b8db545
alexB04676 <157880440+alexB04676@users.noreply.github.com>
2025-02-24 07:25:31 +0330

    model trained
    Trained XGBoost model on extracted features, achieving a 2.5% error margin (RMSE = 0.025) with base parameters. Performance is solid for an untuned model, with further improvements expected through hyperparameter tuning

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from DatasetCleaner import Preprocessor
 import os
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import root_mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
 
 # read the dataset with relative paths and error handling
 try:
@@ -36,8 +40,34 @@ items = preprocess.unique_items_list(df, columns= "model")
 df = preprocess.FrequencyEncoding(df, columns= "model")
 df = preprocess.save_dataframe(df, "C:/Users/ali/Projects/SalesPrediction/Sampled_dataset.csv", file_format= "csv")"""
 
-df = preprocess.OneHotEncoder(df, columns= ["condition", "cylinders", "fuel", "title_status", "transmission", "drive", "size", "type", "paint_color"])
+"""df = preprocess.OneHotEncoder(df, columns= ["condition", "cylinders", "fuel", "title_status", "transmission", "drive", "size", "type", "paint_color"])
 df = preprocess.FrequencyEncoding(df, columns=["manufacturer"])
 df = preprocess.TargetEncoder(df, columns=["state"], target= "price")
 df = preprocess.columns_drop(df, columns=["manufacturer", "model"])
-df = preprocess.save_dataframe(df, output_path= "C:/Users/ali/Projects/SalesPrediction/Sampled_dataset.csv", file_format= "csv")
+df = preprocess.save_dataframe(df, output_path= "C:/Users/ali/Projects/SalesPrediction/Sampled_dataset.csv", file_format= "csv")"""
+
+X = df.drop("price", axis= 1)
+y = df["price"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.1, random_state=42
+)
+
+dtrain_reg = xgb.DMatrix(X_train, y_train)
+dtest_reg = xgb.DMatrix(X_test, y_test)
+
+params = {"booster": "gbtree", "objective": "reg:squarederror", "device": "gpu", "eval_metric": "rmse", "subsample": 0.6}
+
+n = 100
+model = xgb.train(
+   params=params,
+   dtrain=dtrain_reg,
+   num_boost_round=n,
+)
+
+preds = model.predict(dtest_reg)
+
+scaler = MinMaxScaler()
+rmse = root_mean_squared_error(y_test, preds)
+
+print(f"RMSE of the base model: {rmse:.3f}")
