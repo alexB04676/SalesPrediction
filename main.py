19bd4249f3b24148da63003973f129e44e50eebb
alexB04676 <157880440+alexB04676@users.noreply.github.com>
2025-03-02 08:36:00 +0330

    tweaked parameters for improved performance
    tweaked and added a few parameters to increase the model's performance. the RMSE had an improvement from 0.025 to 0.022 (in other words, the error margin was reduced from 2.5% to 2.2%). which is good boost. also saved the model for future use.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import os
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import root_mean_squared_error
-from sklearn.preprocessing import MinMaxScaler
+from tqdm import tqdm
+from joblib import dump
 
 # read the dataset with relative paths and error handling
 try:
@@ -56,18 +57,20 @@ X_train, X_test, y_train, y_test = train_test_split(
 dtrain_reg = xgb.DMatrix(X_train, y_train)
 dtest_reg = xgb.DMatrix(X_test, y_test)
 
-params = {"booster": "gbtree", "objective": "reg:squarederror", "device": "gpu", "eval_metric": "rmse", "subsample": 0.6}
+params = {"booster": "gbtree", "objective": "reg:squarederror", "device": "gpu", "eval_metric": "rmse", "subsample": 0.9
+, "max_depth": 9, "lambda": 1, "learning_rate": 0.05, "colsample_bytree": 0.7}
 
-n = 100
 model = xgb.train(
    params=params,
    dtrain=dtrain_reg,
-   num_boost_round=n,
+   num_boost_round=1200,
 )
 
 preds = model.predict(dtest_reg)
 
-scaler = MinMaxScaler()
 rmse = root_mean_squared_error(y_test, preds)
 
 print(f"RMSE of the base model: {rmse:.3f}")
+
+dump(model, "XGBoostSP.joblib")
+print("Model Saved as 'XGBoostSP.joblib'")
